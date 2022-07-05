b688c2d222826b99f23b583773bcbebeebeabbb3
cheungd <cheungd@cheungd-a02.vmware.com>
2022-07-05 10:27:39 +0800

    rebuild test

--- nodejs/npm/server.js
+++ nodejs/npm/server.js
@@ -7,7 +7,7 @@ app.get('/', (request, response) => {
   response.send(`<!DOCTYPE html>
 <html>
   <head>
-    <title>Powered By Paketo Buildpacks</title>
+    <title>Tanzu Build Service Demo - Powered By Paketo Buildpacks</title>
   </head>
   <body>
     <img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="https://paketo.io/images/paketo-logo-full-color.png"></img>
